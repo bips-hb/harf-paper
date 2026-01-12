7192eb3c04bdf769bc1f77e7cc4fd3a4bc197ca3
fouodo <fouodo@leibniz-bips.de>
2026-01-12 21:17:15 +0100

    Scripts to download tissue Patel's data

---
+++ data/hember-lab/tissue-patel/01-patel.R
@@ -0,0 +1,28 @@
+library(data.table)
+library(SingleCellExperiment)
+library(this.path)
+setwd(dirname(this.path::this.path()))
+source("../create_sce.R")
+### DATA
+d <- read.table("data.txt")
+# select 5 patients
+d <- d[,grepl("MGH26_", colnames(d)) |
+       grepl("MGH264_", colnames(d)) |
+       grepl("MGH28_", colnames(d)) |
+       grepl("MGH29_", colnames(d)) |
+       grepl("MGH30_", colnames(d)) |
+       grepl("MGH31_", colnames(d))]
+
+### ANNOTATIONS
+patients <- unlist(lapply(strsplit(colnames(d), "_"), "[[", 1))
+patients[patients == "MGH264"] <- "MGH26"
+ann <- data.frame(cell_type1 = patients)
+rownames(ann) <- colnames(d)
+
+### SINGLECELLEXPERIMENT
+sceset <- create_sce_from_logcounts(d, ann)
+# Built a data.table
+sceset_dt <- as.data.table(t(logcounts(sceset)))
+sceset_dt$cell_type <- sceset$cell_type1
+fwrite(x = sceset_dt,
+       file = "patel_not_filtered.txt", sep = "\t")
