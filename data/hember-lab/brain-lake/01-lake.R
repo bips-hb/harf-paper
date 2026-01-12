311f6084fca85e3b32f5c7d667e33eb31ae510a8
fouodo <fouodo@leibniz-bips.de>
2026-01-12 21:12:34 +0100

    Add scripts to download brain Lake's data

---
+++ data/hember-lab/brain-lake/01-lake.R
@@ -0,0 +1,68 @@
+library(data.table)
+library(SingleCellExperiment)
+library(this.path)
+setwd(dirname(this.path::this.path()))
+source("../create_sce.R")
+# Preprocess Lake et al. human brain data
+### DATA
+# http://genome-tech.ucsd.edu/ZhangLab/index.php/data/epigenomics-and-transcriptomics/sns/
+# Lake et al. (2016) Neuronal subtypes and diversity revealed by single-nucleus RNA sequencing of the human brain. Science. 352 (6293): 1586-1590
+# Own website
+#Prefrontal cortex - BA10
+#Frontal cortex (eye fields) - BA8
+#Auditory cortex (speech) - BA21
+#Auditory cortex (Wernicke's area) - BA22
+#Auditory cortex - BA41/42
+#Visual cortex - BA17
+# Problems: duplicate row names
+# Human
+x1 = read.delim("Lake-2016_Gene_TPM.dat", "\t", header=F, stringsAsFactors=FALSE)
+ann <- read.table("Lake-2016_Gene_TPM_Sample-annotation.txt", header=T)
+gene_names <- x1[,1]
+cell_names <- x1[1,]
+cell_names <- as.character(unlist(cell_names))
+cell_names <- cell_names[-1]
+gene_names <- gene_names[-1]
+x1 <- x1[-1,-1]
+# not log-transformed tpms
+exclude = duplicated(gene_names)
+keep_cells = cell_names %in% ann[,2]
+x1 <- x1[,keep_cells]
+cell_names <- cell_names[keep_cells]
+colnames(x1) <- cell_names;
+reorder <- order(colnames(x1))
+x1 <- x1[,reorder]
+
+### ANNOTATIONS
+ann <- ann[ann[,2] %in% colnames(x1),]
+ann<-ann[order(ann[,2]),]
+SOURCE <- as.character(unlist(ann[,4]))
+BATCH <- as.character(unlist(ann[,4]))
+SOURCE[SOURCE=="BA10"] = "Prefrontal cortex"
+SOURCE[SOURCE=="BA8"] = "Frontal cortex"
+SOURCE[SOURCE=="BA21"] = "Auditory cortex (speech)"
+SOURCE[SOURCE=="BA22"] = "Auditory cortex (Wernicke)"
+SOURCE[SOURCE=="BA41"] = "Auditory cortex"
+SOURCE[SOURCE=="BA17"] = "Visual cortex"
+TYPE <- as.character(unlist(ann[3]))
+AGE <- rep("51yo", times=length(BATCH))
+stuff <- sub( "_S.+","", colnames(x1));
+stuff <- matrix(unlist(strsplit(stuff, "_")), ncol=2, byrow=T)
+WELL <- stuff[,2]
+PLATE <- stuff[,1]
+DATA <- apply(x1, 1, as.numeric)
+DATA<- t(DATA)
+colnames(DATA) <- colnames(x1)
+DATA <- DATA[!exclude, ]
+rownames(DATA) <- gene_names[!exclude]
+ANN <- data.frame(Species=rep("Homo sapiens", times=length(TYPE)), cell_type1=TYPE, Source=SOURCE, age=AGE, WellID=WELL, batch=BATCH, Plate=PLATE)
+rownames(ANN) <- colnames(x1)
+
+### SINGLECELLEXPERIMENT
+sceset <- create_sce_from_normcounts(DATA, ANN)
+# Built a data.table
+sceset_dt <- as.data.table(t(logcounts(sceset)))
+sceset_dt$cell_type <- sceset$cell_type1
+
+fwrite(x = sceset_dt,
+       file = "lake_not_filtered.txt", sep = "\t")
---
+++ data/hember-lab/brain-lake/02-lake-processing.R
@@ -0,0 +1,37 @@
+library(data.table)
+library(SingleCellExperiment)
+library(this.path)
+setwd(dirname(this.path::this.path()))
+# Preprocess Baron et al. human pancreas data
+
+cell_data <- fread("lake_not_filtered.txt")
+cell_annotation <- data.table(cell_type = cell_data$cell_type)
+cell_data$cell_type <- NULL
+# Create SingleCellExperiment object
+sce <- SingleCellExperiment(
+  assays = list(counts = t(as.matrix(cell_data)))
+)
+sce$cell_type <- cell_annotation$cell_type
+# Frequency filtering (FRQ): filter cells genes expressed in less than 6% of the cells
+freq_threshold <- 0.06
+gene_freq <- rowSums(assay(sce) > 0) / ncol(sce)
+sce <- sce[gene_freq >= freq_threshold, ]
+# Highly expressed genes (HiE): select genes with expression above the 90th percentile
+n_genes <- nrow(counts(sce))
+top_n <- ceiling(0.10 * n_genes)
+hie_matrix <- apply(counts(sce), 2, function(x) {
+  idx <- order(x, decreasing = TRUE)[seq_len(top_n)]
+  hie <- logical(length(x))
+  hie[idx] <- TRUE
+  hie
+})
+rownames(hie_matrix) <- rownames(counts)
+min_cells <- ceiling(0.10 * ncol(sce))
+keep_genes <- rowSums(hie_matrix) >= min_cells
+sce <- sce[keep_genes, ]
+
+# Save sce as data.table
+sce_dt <- as.data.table(t(assay(sce)))
+sce_dt[, cell_type := sce$cell_type]
+fwrite(sce_dt, "brain_lake_processed_data.csv")
+
