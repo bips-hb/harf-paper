a38cc3ce36e22c49f5869a1816fbb45fbd799fa3
fouodo <fouodo@leibniz-bips.de>
2026-01-15 11:48:23 +0100

    Update data preprocessing flow

--- R-code/02-preproceed-hember-data.R
+++ R-code/02-preproceed-hember-data.R
@@ -21,3 +21,7 @@ source(file.path(this.dir(), "01-prepare-hember-data/01-patel.R"),
        chdir = TRUE)
 source(file.path(this.dir(), "01-prepare-hember-data/02-patel-processing.R"),
        chdir = TRUE)
+source(file.path(this.dir(), "01-prepare-hember-data/01-deng.R"),
+       chdir = TRUE)
+source(file.path(this.dir(), "01-prepare-hember-data/02-deng-processing.R"),
+       chdir = TRUE)
